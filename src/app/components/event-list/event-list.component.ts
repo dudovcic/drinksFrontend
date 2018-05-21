7508ac54bd633b1a2398c4fd06db9f97cd66e219
dudovcic <denis.udovcic.ext@siemens-convergence.com>
2018-05-21 13:20:21 +0200

    pipe for times added

--- src/app/components/event-list/event-list.component.ts
+++ src/app/components/event-list/event-list.component.ts
@@ -18,7 +18,6 @@ export class EventListComponent {
   
   click(event: Event) {
     this.clickEvent.emit(event.location)
-    //scroll(0, 0) - scroll to map when component is clicked, but it irritates UX a bit
   }
 
   onChange() {
